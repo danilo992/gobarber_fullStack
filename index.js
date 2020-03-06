b115b96930d965311e53b3bde6bffdb03a2a75ab
danilo992 <tadeudanilo46@gmail.com>
2020-03-06 07:27:38 -0500

    understanding a little bit of Query, Route e body.

--- index.js
+++ index.js
@@ -3,8 +3,22 @@ const express = require('express');
 const app = express();
 
 app.get('/teste', (req, res) => {
+
+    const name = req.query.name;
+
     return res.json({
-         message: 'Hello Word',
+         message: `Hello ${name}`,
+         contat: '11-000-0000'
+         });
+})
+
+
+app.get('/users/:id', (req, res) => {
+
+    const { id } = req.params;
+
+    return res.json({
+         message: `user ${id}`,
          contat: '11-000-0000'
          });
 })
