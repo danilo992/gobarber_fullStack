867689232a82cb96ad1bd08112e89d060a229a85
danilo992 <tadeudanilo46@gmail.com>
2020-03-06 08:39:57 -0500

    update

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
-<p align="center"><img src="https://raw.githubusercontent.com/radaelilucca/gobarber/master/src/assets/GoBarber.png" alt="gobarber" align="center"/>
-  </p>
-  <p align="center"> </br><img src="https://david-dm.org/radaelilucca/gobarber.svg" alt="dep"/></p>
+<p align="center">
+  <img 
+    src="https://raw.githubusercontent.com/radaelilucca/gobarber/master/src/assets/GoBarber.png"           alt="gobarber" align="center"/>
+</p>
+<p align="center">
+  </br><img 
+    src="https://david-dm.org/radaelilucca/gobarber.svg" 
+    alt="dep"/>
+</p>
 
 # gobarber_fullStack
 
-## Backend: Javascript => Node.js 
+## Backend: Javascript:{
+ Node.js e Insomnia
+}
 >Yarn, Express
--- index.js
+++ index.js
@@ -2,46 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.get('/teste', (req, res) => {
+app.use(express.json());
 
-    const name = req.query.name;
+const users = ['car', 'file', 'package'];
 
-    return res.json({
-         message: `Hello ${name}`,
-         contat: '11-000-0000'
-         });
-})
-
-/*
-app.get('/users/:id', (req, res) => {
-
-    const { id } = req.params;
-
-    return res.json({
-         message: `user ${id}`,
-         contat: '11-000-0000'
-         });
-})
-
-app.get('/user/:id', (req, res) => {
-
-    const { id } = req.params;
-
-    return res.json({
-         message: `user ${id}`,
-         contat: '11-000-0000'
-         });
-})
-*/
-const users = ['Di', 'Dolla', 'Mony']
-
-app.get('/users/:index', (req, res) => {
-
-    const { index } = req.params;
-
-    return res.json(
-         users[index]
-         );
+app.get('/users', (req, res) => {
+    return res.json(users);
 })
 
 app.listen(3000);
