a587284a2ab5d84c53199c789a5021c3932555bd
danilo992 <tadeudanilo46@gmail.com>
2020-03-09 02:07:29 -0400

    CRUD => Create, Delete, Read, Update

--- index.js
+++ index.js
@@ -10,4 +10,33 @@ app.get('/users', (req, res) => {
     return res.json(users);
 })
 
+app.get('/users/:index', (req, res) => {
+    const { index } = req.params;
+
+    return res.json(users[index]);
+})
+
+app.post('/users', (req, res) => {
+    const { name } = req.body;
+
+    users.push(name);
+
+    return res.json(users);
+})
+
+app.put('/users/:index', (req ,res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+
+    users[index] = name;
+
+    return res.json(users);
+})
+
+app.delete('/users/:index', (req, res) => {
+    const { index } = req.params;
+
+    users.splice(index, 1);
+    return res.json(users);
+})
 app.listen(3000);
