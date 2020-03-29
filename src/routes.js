60c3a1c8d4b4ad118aa938a03f1a01266a072c77
danilo992 <tadeudanilo46@gmail.com>
2020-03-28 20:04:33 -0400

    add sequelize

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
 
-    return res.json({ message: 'Hello World' });
+    return res.json({ message: 'Hello World sky 3' });
 })
 
-module.exports = routes;
+export default routes;
