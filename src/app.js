faa6f3602b463ed136f9a4d8ee0aa1b2d4d39b2c
danilo992 <tadeudanilo46@gmail.com>
2020-04-02 11:16:57 -0400

    error git pull

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import routes from './routes';
-<<<<<<< HEAD
+
 
 import './database';
-=======
->>>>>>> 1f95813345d59e09ea492d8d5d36996915da5544
+
 
 import './database';
 
